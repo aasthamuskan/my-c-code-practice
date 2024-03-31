52a94bbcd5304c0504e96c5998ac6d3e701088b1
aasthamuskan <aasthamuskan9430@gmail.com>
2024-03-31 20:39:14 +0530

    day 2

---
+++ patternprinting/starprinting/pattern1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    for (int i =1;i<=3;i++) {//outer loop signifies no of line
+        for (int i=1;i<=5;i++){//inner loop signifies ek line me kya kya kam hoga ;
+            printf("*");}
+            printf("\n");
+       }   return 0;   
+      
+    
+}
---
+++ patternprinting/starprinting/pattern2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    for (int i =1;i<=5;i++) {//outer loop signifies no of line
+        for (int i=1;i<=5;i++){//inner loop signifies ek line me kya kya kam hoga ;
+            printf("*");}
+            printf("\n");
+       }   return 0;   
+      
+}
