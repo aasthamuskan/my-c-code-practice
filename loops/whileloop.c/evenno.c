7ba0cd8b32401c6f1626513c21e14d31a9b14a8e
aasthamuskan <aasthamuskan9430@gmail.com>
2024-04-03 22:33:01 +0530

    day 5

---
+++ loops/whileloop.c/evenno.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,i;
+    printf("enter the first number: ");
+    scanf("%d",&a);
+    i=1;
+    while(i<=a){
+        if(i%2==0){;
+        printf("%d",i);
+    }
+    i++;
+}return 0;
+}
--- loops/whileloop.c/firstnnaturalno.c
+++ loops/whileloop.c/firstnnaturalno.c
@@ -2,5 +2,11 @@
 int main(){
     int n;
     printf("\nEnter value of N: ");
-
+    scanf("%d",&n);
+    int i=1;
+    while(i<=n){
+        printf("%d",i);
+            i++;
+    }
+    return 0;
 }
