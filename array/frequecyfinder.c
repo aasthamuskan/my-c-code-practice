2d0f9165410f4f238ea28d9206f027d6bb7ef6bd
aasthamuskan <aasthamuskan9430@gmail.com>
2024-04-06 20:49:07 +0530

    completed

--- array/frequecyfinder.c
+++ array/frequecyfinder.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 int main(){
-    int a,b,c,d;
+    int a[10],i,f=0,key;
+    for (i=0;i<0;i++){
+        printf("\nEnter Number: ");
+        scanf("%d",&a[i]);
+        }
+    printf("Enter the number you want frequency:");
+    scanf("%d",&key);
+    for(i=0;i<=9;i++)
+      if(a[i]==key)
+          f++;
+    printf("\n%d Appears %d Times In The Array.", key, f); 
+    return 0;  
     
+
 }
