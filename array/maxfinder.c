b423c8737e2040ebaaa95b42f00dde3cb5b14115
aasthamuskan <aasthamuskan9430@gmail.com>
2024-04-08 20:53:07 +0530

    day 9

---
+++ array/maxfinder.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a[10],i,max;
+    for(i=0;i<10;i++){
+        printf("enter the number: ");
+        scanf(" %d",&a[i]);
+    }
+    max = a[0];
+    for(i=0;i<10;i++)
+    if(a[i]>max)
+        max =a[i];
+        printf("\nmax number is %d .",max);
+    
+    return 0;
+}
