2a63fd4a644994c711fe985310b225edfc3ff919
aasthamuskan <aasthamuskan9430@gmail.com>
2024-04-12 17:16:26 +0530

    day12

--- array/1-D array/maxfinder.c
+++ array/1-D array/maxfinder.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main(){
     int a[10],i,max;
-    for(  i=0;i<10;i++){
-        printf("enter the number: ");
+    for(  i=0; i<10;i++){
+        printf("enter the  number: ");
         scanf(" %d",&a[i]);
     }
     max = a[0];
     for(i=0;i<10;i++)
     if(a[i]>max)
         max =a[i];
-        printf("\n max number is %d .",max);
+        printf("\n max  number is %d .",max);
     
     return 0;
 }
