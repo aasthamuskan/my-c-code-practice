ad95c08a7ff4f48fa3f15048816018ebb62a465e
aasthamuskan <aasthamuskan9430@gmail.com>
2024-04-18 23:06:48 +0530

    day18

--- pointers/pointersclass.c
+++ pointers/pointersclass.c
@@ -3,7 +3,7 @@ int main(){
     int *ptr=NULL;
     int a=10;
     printf("%u",ptr);
-    ptr= &a;
-    printf("\n%d %u\n",*ptr,ptr);
+     ptr= &a;
+     printf("\n%d %u\n",*ptr,ptr);
     return 0;
 }
