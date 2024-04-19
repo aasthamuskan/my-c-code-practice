5fc7b2e33a2914b6f588f5705cc886a0af66dd14
aasthamuskan <aasthamuskan9430@gmail.com>
2024-04-19 23:17:01 +0530

    daay19

--- pointers/evencheck.c
+++ pointers/evencheck.c
@@ -2,7 +2,7 @@
 int main(){
     int a,*p;
     printf("\nEnter the no to check for odd/even:  ");
-    scanf("%d",&a);
+    scanf("%d" ,&a);
     p = &a;                   //address of variable 'a' is stored in pointer variable 'p'
     if(*p % 2 ==  0)            //value at address stored in 'p' is checked for evenness by using dereferencing operator *
        printf("even");
--- pointers/pointersclass.c
+++ pointers/pointersclass.c
@@ -2,7 +2,7 @@
 int main(){
     int *ptr=NULL;
     int a=10;
-    printf("%u",ptr);
+     printf("%u",ptr);
      ptr= &a;
      printf("\n%d %u\n",*ptr,ptr);
     return 0;
