b2ebf7876f4869404e503fb9e0e90c0ba1899d3f
aasthamuskan <aasthamuskan9430@gmail.com>
2024-05-03 22:27:23 +0530

    day 25

---
+++ string/basic.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    char name[50];
+    name == "aastha muskan";
+    printf("\nyour name is %s",name);
+    return 0;
+}
---
+++ string/userdefine.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char a[50];
+    printf("\nEnter Your Name:");
+    scanf("%s",&a);
+    printf("\nYour name is %s",a);
+    return 0;
+}
